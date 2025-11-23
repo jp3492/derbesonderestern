30b4ca9dfb099ddc651380c328c27ac82fd9efa8
Jan-Philipp Marks <jpm3492@gmail.com>
2025-11-23 12:15:14 +0100

    fxi pipeline assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   i18n: {
-    defaultLocale: 'de',
-    locales: ['en', 'de'],
+    defaultLocale: "de",
+    locales: ["en", "de"],
+  },
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/passthrough",
+    },
   },
 });
